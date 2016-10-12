fa83df3e8be0d3a82850e9835ecc905936a825d3
Hannah Salmon <hr.salmon@gmail.com>
2016-10-13 11:59:30 +1300

    Add tests

--- plugin.rb
+++ plugin.rb
@@ -6,18 +6,22 @@ module Plugins
         plugin.use_component :group_progress_card, outlet: :before_group_column_right
         plugin.use_translations 'config/locales', :group_progress_card
 
-        plugin.use_test_route(:setup_progress_card) do
-          veronica = User.create!(name: 'Veronica Sawyers',
-                                  email: 'veronicasawyers@example.com',
-                                  username: 'veronicasawyers',
-                                  password: 'passwordpassword',
-                                  angular_ui_enabled: true).tap {|u| u.experienced! 'welcomeModal'}
-          group = Group.new(name: 'New group')
-          GroupService.create(group: group, actor: veronica)
-          group.update_attribute(:enable_experiments, true)
-          group.add_admin! veronica
-          sign_in veronica
-          redirect_to group_path(group)
+        plugin.use_test_route(:setup_progress_card_coordinator) do
+          GroupService.create(group: test_group, actor: patrick)
+          test_group.update_attribute(:enable_experiments, true)
+          test_group.update_attribute(:description, 'Here is a group description')
+          test_group.cover_photo = File.new("#{Rails.root}/spec/fixtures/images/strongbad.png")
+          test_group.save
+          patrick.avatar_kind = 'gravatar'
+          patrick.save
+          sign_in patrick
+          test_discussion
+          redirect_to group_url(test_group)
+        end
+        plugin.use_test_route(:setup_progress_card_member) do
+          test_group.update_attribute(:enable_experiments, true)
+          sign_in jennifer
+          redirect_to group_url(test_group)
         end
       end
     end
--- test/group_progress_card_spec.coffee
+++ test/group_progress_card_spec.coffee
@@ -1,17 +1,30 @@
 describe 'Group progress card', ->
 
-  page = require './helpers/page_helper.coffee'
-
-  beforeEach ->
-    page.loadPath 'setup_progress_card'
+  page = require '../../../angular/test/protractor/helpers/page_helper.coffee'
+  staticPage = require '../../../angular/test/protractor/helpers/static_page_helper.coffee'
 
   it 'is only visible to group coordinators', ->
-    page.expectText '.group-progress-card' 'Activate your group'
+    page.loadPath 'setup_progress_card_coordinator'
+    page.expectText '.group-progress-card', 'ACTIVATE YOUR GROUP'
 
-  it 'is not visible to non-coordinators', ->
+  it 'is only visible to group coordinators', ->
+    page.loadPath 'setup_progress_card_member'
+    page.expectNoText '.group-page', 'ACTIVATE YOUR GROUP'
 
-  it 'adds a strikethrough and a tick to completed tasks', ->
+  it 'adds a tick to completed tasks', ->
+    page.loadPath 'setup_progress_card_coordinator'
+    page.expectText '.group-progress-card', '✅'
 
   it 'displays a celebratory message when setup is complete', ->
+    page.loadPath 'setup_progress_card_coordinator'
+    page.clickLast '.thread-preview__link'
+    page.click '.start-proposal-button__button'
+    page.fillIn '.proposal-form__title-field', 'New proposal'
+    page.click '.proposal-form__start-btn'
+    page.click '.group-theme__name--compact a'
+    page.expectText '.group-progress-card', "Nice! You're group is good to go!"
 
   it 'can be dismissed', ->
+    page.loadPath 'setup_progress_card_coordinator'
+    page.click '.group-progress-card__dismiss'
+    page.expectNoText '.group-page', 'ACTIVATE YOUR GROUP'
