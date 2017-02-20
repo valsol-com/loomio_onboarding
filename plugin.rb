12ec75a94fac5c84dd8901663f61288131255b38
James Kiesel <james.kiesel@gmail.com>
2017-02-21 12:43:16 +1300

    Use new testing framework

--- plugin.rb
+++ plugin.rb
@@ -11,30 +11,30 @@ module Plugins
         plugin.use_asset 'components/decorators/group_page_controller_decorator.coffee'
 
         plugin.use_test_route(:setup_progress_card_coordinator) do
-          GroupService.create(group: test_group, actor: patrick)
+          GroupService.create(group: create_group, actor: patrick)
           test_subgroup = Group.new(name: 'Johnny sub',
-                                    parent: test_group,
+                                    parent: create_group,
                                     discussion_privacy_options: 'public_or_private',
                                     group_privacy: 'closed')
           GroupService.create(group: test_subgroup, actor: patrick)
-          test_group.update_attribute(:enable_experiments, true)
-          test_group.update_attribute(:description, 'Here is a group description')
-          test_group.cover_photo = File.new("#{Rails.root}/spec/fixtures/images/strongbad.png")
-          test_group.save
+          create_group.update_attribute(:enable_experiments, true)
+          create_group.update_attribute(:description, 'Here is a group description')
+          create_group.cover_photo = File.new("#{Rails.root}/spec/fixtures/images/strongbad.png")
+          create_group.save
           patrick.avatar_kind = 'gravatar'
           patrick.save
           sign_in patrick
-          test_discussion
-          redirect_to group_url(test_group)
+          create_discussion
+          redirect_to group_url(create_group)
         end
         plugin.use_test_route(:setup_progress_card_member) do
-          test_group.update_attribute(:enable_experiments, true)
+          create_group.update_attribute(:enable_experiments, true)
           sign_in jennifer
-          redirect_to group_url(test_group)
+          redirect_to group_url(create_group)
         end
         plugin.use_test_route(:setup_group_with_intro_carousel) do
           sign_in emilio
-          redirect_to group_url(test_group)
+          redirect_to group_url(create_group)
         end
       end
     end
