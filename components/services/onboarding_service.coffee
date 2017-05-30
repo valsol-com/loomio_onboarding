3fcfde484069d95f8d98b622a3f7c5f4a61097fe
James Kiesel <james.kiesel@gmail.com>
2017-05-30 20:47:01 +1200

    Remove intro carousel

--- components/services/onboarding_service.coffee
+++
@@ -1,9 +0,0 @@
-angular.module('loomioApp').factory 'OnboardingService', (Session) ->
-  new class OnboardingService
-
-    showIntroCarousel: (group) ->
-      Loomio.pluginConfig('loomio_onboarding') &&
-      group.isParent() &&
-      Session.user().isMemberOf(group) &&
-      !Session.user().hasExperienced("introductionCarousel") &&
-      group.createdAt.isAfter(moment("2016-11-05"))
--- plugin.rb
+++ plugin.rb
@@ -4,11 +4,8 @@ module Plugins
       setup! :loomio_onboarding do |plugin|
         plugin.enabled = true
         plugin.use_component :group_progress_card, outlet: [:before_group_page_column_right, :before_thread_page_column_right]
-        plugin.use_component :introduction_carousel, outlet: :before_group_page
 
         plugin.use_translations 'config/locales', :loomio_onboarding
-
-        plugin.use_asset 'components/decorators/group_page_controller_decorator.coffee'
 
         plugin.use_test_route(:setup_progress_card_coordinator) do
           GroupService.create(group: create_group, actor: patrick)
@@ -17,7 +14,6 @@ module Plugins
                                     discussion_privacy_options: 'public_or_private',
                                     group_privacy: 'closed')
           GroupService.create(group: test_subgroup, actor: patrick)
-          create_group.update_attribute(:enable_experiments, true)
           create_group.update_attribute(:description, 'Here is a group description')
           create_group.cover_photo = File.new("#{Rails.root}/spec/fixtures/images/strongbad.png")
           create_group.save
@@ -27,13 +23,9 @@ module Plugins
           create_discussion
           redirect_to group_url(create_group)
         end
+
         plugin.use_test_route(:setup_progress_card_member) do
-          create_group.update_attribute(:enable_experiments, true)
           sign_in jennifer
-          redirect_to group_url(create_group)
-        end
-        plugin.use_test_route(:setup_group_with_intro_carousel) do
-          sign_in emilio
           redirect_to group_url(create_group)
         end
       end
